a6e2bc7aa9d9564ce7d1b4e6c33a369d38b26f4d
Samuel Tyler <samuel.e.tyler@gmail.com>
2023-04-10 09:15:42 -0400

    attempt to add backup picture for video

--- sections/initialize.php
+++ sections/initialize.php
@@ -37,3 +37,7 @@ $CURRENT_FILE_NAME = basename($_SERVER['SCRIPT_FILENAME']);
         });
     });
 </script>
+
+<!--wait until css loaded to show page 
+    from https://stackoverflow.com/questions/4172281/force-browsers-to-load-css-before-showing-the-page-->
+<div id="loadOverlay" style="background-color:#000; position:absolute; top:0px; left:0px; width:100%; height:100%; z-index:2000;"></div>
