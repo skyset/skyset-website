48ae9c9aec48da6440c7cc118b5a79e5c25d2e70
Samuel Tyler <samuel.e.tyler@gmail.com>
2023-11-21 12:41:34 -0500

    composer+php deployment setup

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'samue/skyset-website',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '0974d86a9e99f0e168bb2d6274fab5226227a789',
+        'type' => 'website',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'samue/skyset-website' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '0974d86a9e99f0e168bb2d6274fab5226227a789',
+            'type' => 'website',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
