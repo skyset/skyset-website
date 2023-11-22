04ff48868309d087b87afe59de46e11f1347cc3b
Samuel Tyler <samuel.e.tyler@gmail.com>
2023-11-21 23:10:06 -0500

    create deploy.php

---
+++ deploy.php
@@ -0,0 +1,18 @@
+<?php
+
+$dir = new DirectoryIterator(dirname('pages'));
+foreach ($dir as $fileinfo) {
+    if (!$fileinfo->isDot() && !$fileinfo->isDir()) {
+        $staticfile = fopen($fileinfo->getBasename('.php') . '.html', 'w');
+
+        ob_start();
+        include $fileinfo->getFilename();
+        $content = ob_get_contents();
+        ob_end_clean();
+
+        fwrite($staticfile, $content);
+        fclose($staticfile);
+    }
+}
+
+?>
